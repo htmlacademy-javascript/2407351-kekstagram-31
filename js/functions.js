07c421c0e0abd49c8b2e809c4b46b4ee6109ddf5
Dmitriy Korotkov <dimka-13-01@mail.ru>
2024-03-28 21:23:31 +0300

    Исправляет ошибки линтера

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,8 @@
 function isMeetingWithinWorkingHours(startWorking, endWorking, startMeeting, duration) {
   // Преобразование времени в минуты
   const timeToMinutes = (time) => {
-      const [hours, minutes] = time.split(':').map(Number);
-      return hours * 60 + minutes;
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
   };
 
   const startWorkingMinutes = timeToMinutes(startWorking);
@@ -11,19 +11,19 @@ function isMeetingWithinWorkingHours(startWorking, endWorking, startMeeting, dur
 
   // Проверка, что время начала встречи находится в пределах рабочего дня
   if (startMeetingMinutes < startWorkingMinutes || startMeetingMinutes > endWorkingMinutes) {
-      return false;
+    return false;
   }
 
   // Проверка, что время окончания встречи находится в пределах рабочего дня
   if (startMeetingMinutes + duration > endWorkingMinutes) {
-      return false;
+    return false;
   }
 
   return true;
 }
 
-console.log(isMeetingWithinWorkingHours('08:00', '17:30', '14:00', 90));
-console.log(isMeetingWithinWorkingHours('8:0', '10:0', '8:0', 120));
-console.log(isMeetingWithinWorkingHours('08:00', '14:30', '14:00', 90));
-console.log(isMeetingWithinWorkingHours('14:00', '17:30', '08:0', 90));
-console.log(isMeetingWithinWorkingHours('8:00', '17:30', '08:00', 900));
+window.console.log(isMeetingWithinWorkingHours('08:00', '17:30', '14:00', 90));
+window.console.log(isMeetingWithinWorkingHours('8:0', '10:0', '8:0', 120));
+window.console.log(isMeetingWithinWorkingHours('08:00', '14:30', '14:00', 90));
+window.console.log(isMeetingWithinWorkingHours('14:00', '17:30', '08:0', 90));
+window.console.log(isMeetingWithinWorkingHours('8:00', '17:30', '08:00', 900));
