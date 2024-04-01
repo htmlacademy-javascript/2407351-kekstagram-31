5a44bdb2c388d4c0109d764912aad58f4aaedcd1
Keks <keks@htmlacademy.ru>
2024-04-01 03:58:50 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/photos/thumbnails.js
@@ -0,0 +1,35 @@
+import { createdPhotos } from '../mocks/index.js';
+import { findTemplate, renderPack } from '../utils/dom.js';
+import { openBigPicture } from './big-picture.js';
+
+const template = findTemplate('picture');
+
+const picturesContainer = document.querySelector('.pictures');
+
+const createThumbnail = (photo) => {
+  const thumbnail = template.cloneNode(true);
+
+  thumbnail.href = photo.url;
+  thumbnail.dataset.pictureId = photo.id;
+
+  const image = thumbnail.querySelector('.picture__img');
+
+  image.src = photo.url;
+  image.alt = photo.description;
+
+  thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = photo.likes;
+
+  return thumbnail;
+};
+
+renderPack(createdPhotos, createThumbnail, picturesContainer);
+
+picturesContainer.addEventListener('click', (evt) => {
+  const currentPicture = evt.target.closest('.picture');
+  if (currentPicture) {
+    evt.preventDefault();
+    openBigPicture(currentPicture.dataset.pictureId);
+  }
+});
+
